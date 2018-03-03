ed1558f21ed4358633dc0f1918f58f4e54b43ed8
Sean Cranston <scranston24@Outlook.com>
2018-03-03 09:42:43 -0500

    added

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Tells node that we are creating an "express" app
+var app = express();
